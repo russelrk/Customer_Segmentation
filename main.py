747fb6992b3f69ad012c77316df837aafca44d5b
russelrk <49132622+russelrk@users.noreply.github.com>
2024-01-08 15:26:21 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import argparse
+from load_data import load_data
+from data_preprocess import preprocess_and_clean_data
+from train_logistic_regression import train_logistic_regression_model
+from explore_data import explore_data
+
+def main(data_path):
+    # Load your data
+    df = load_data(data_path)
+
+    # EDA
+    explore_data(df)
+
+    # Preprocess and clean the data
+    df_cleaned = preprocess_and_clean_data(df)
+
+    # Train the logistic regression model
+    classification_report_result = train_logistic_regression_model(df_cleaned)
+    print(classification_report_result)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Perform EDA and logistic regression.')
+    parser.add_argument('data_path', type=str, help='Path to the data file (e.g., sample_data/Train.csv)')
+    args = parser.parse_args()
+    
+    data_path = args.data_path
+    main(data_path)
